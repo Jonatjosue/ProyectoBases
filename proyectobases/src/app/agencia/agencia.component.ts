09d2e40ca272273236add3d19ed1d5e4afdb54da
SandraFPerez <sdrfabiola.perez@gmail.com>
2024-10-26 09:06:54 -0600

    Desarrollo Dashboard

---
+++ proyectobases/src/app/agencia/agencia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agencia',
+  standalone: true,
+  imports: [],
+  templateUrl: './agencia.component.html',
+  styleUrl: './agencia.component.css'
+})
+export class AgenciaComponent {
+
+}
--- proyectobases/src/app/app.routes.ts
+++ proyectobases/src/app/app.routes.ts
@@ -1,9 +1,19 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { AdminComponent } from './admin/admin.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { AgenciaComponent } from './agencia/agencia.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { VehiculoComponent } from './vehiculo/vehiculo.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
   {path: 'menu', component: MenuComponent},
+  {path: 'admin', component: AdminComponent},
+  {path: 'admin/cliente', component: ClienteComponent},
+  {path: 'admin/empleado', component: ClienteComponent},
+  {path: 'admin/vehiculo', component: ClienteComponent},
+  {path: 'admin/agencia', component: AgenciaComponent}
 
 ];
